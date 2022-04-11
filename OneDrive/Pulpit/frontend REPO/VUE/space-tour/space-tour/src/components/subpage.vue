20c863de9190090aaa36146aa6ecc963d4b2c1da
Borys <borys.baca@gmail.com>
2022-04-11 23:45:17 +0200

    struggling with img src from json

---
+++ OneDrive/Pulpit/frontend REPO/VUE/space-tour/space-tour/src/components/subpage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="subpage__img">
+    <img :src="require(data.images.png)" :alt="data.name" />
+  </div>
+  <div class="subpage__title">{{ data.name }}</div>
+  <div class="subpage__text">
+    <p>{{ data.description }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "subpage",
+  props: ["data"],
+};
+</script>
